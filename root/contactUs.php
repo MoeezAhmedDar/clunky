b50a2a72d10629ffe57363814a6c08f7583a8bda
Moeez384 <moeezahmed448@gmail.com>
2020-09-12 16:28:07 +0500

    Final Upload

--- root/contactUs.php
+++ root/contactUs.php
@@ -103,7 +103,7 @@ box-sizing:border-box;
 
 
 
-<nav class="navbar navbar-expand-lg navbar-light ">
+<nav class="navbar navbar-expand-lg navbar-light sticky-top">
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
